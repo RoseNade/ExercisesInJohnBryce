f9ca09c203cc6fdfc923d19820c00d83c4f8b641
Tomer Shmueli <satlikus@gmail.com>
2022-01-27 10:36:24 +0200

    solved properly

---
+++ src/Arrays2d/ex9.java
@@ -0,0 +1,33 @@
+package Arrays2d;
+
+import java.util.Arrays;
+
+public class ex9 {
+    public static void main(String[] args) {
+        int[][] arr = new int[10][10];
+        for (int i = 1; i <= 5; i++) {
+            fill(arr, i);
+        }
+        print(arr);
+    }
+
+    public static void fill(int[][] arr, int num){
+        for (int i = num - 1; i < arr.length - num + 1; i++) {
+            for (int j = num - 1; j < arr[i].length - num + 1; j++) {
+                if (i == num - 1 || i == arr.length - num) {
+                    arr[i][j] = num;
+                    arr[j][i] = num;
+                }
+            }
+        }
+    }
+
+    public static void print(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
